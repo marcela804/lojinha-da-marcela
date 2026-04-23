30e5bb3169258e5de55264b62c69d196861a8973
marcela804 <01marcelarosa01@gmail.com>
2026-04-23 10:16:26 -0300

    Create cadastrar_endereco.py
    Esse código cria uma página HTML com um formulário para cadastrar endereços, permitindo inserir dados e enviá-los para o sistema salvar no banco.

---
+++ cadastrar_endereco.py
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastrar Endereço</title>
+</head>
+<body>
+
+    <h1>Cadastrar Endereço</h1>
+
+    <form action="/cadastrar_endereco" method="POST">
+
+        <label>Rua:</label><br>
+        <input type="text" name="rua" required><br><br>
+
+        <label>Número:</label><br>
+        <input type="text" name="numero" required><br><br>
+
+        <label>Cidade:</label><br>
+        <input type="text" name="cidade" required><br><br>
+
+        <label>CEP:</label><br>
+        <input type="text" name="cep" placeholder="12345-678" maxlength="10"><br><br>
+
+        <button type="submit">Salvar Endereço</button>
+
+    </form>
+
+    <br>
+
+    <a href="/">⬅ Voltar</a>
+
+</body>
+</html>
